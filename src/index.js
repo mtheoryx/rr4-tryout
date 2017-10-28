f2b5762f363258c68c98f83f266a353010c21df1
David Poindexter <david.poindexter@blackboard.com>
2017-10-28 14:41:44 -0400

    Basic router setup

--- src/index.js
+++ src/index.js
@@ -5,3 +5,30 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+// Enable HMR
+if (module.hot) {
+    module.hot.accept('./App', () => 
+        ReactDOM.render(<App />, document.getElementById('root')))
+}
+  
+  //Using Redux
+  // import { createStore } from 'redux'
+  //
+  // import rootReducer from './reducers'
+  //
+  // const configureStore = () => {
+  //   const store = createStore(rootReducer)
+  //
+  //   if (process.env.NODE_ENV !== "production") {
+  //     if (module.hot) {
+  //       module.hot.accept('./reducers', () => {
+  //         store.replaceReducer(rootReducer)
+  //       })
+  //     }
+  //   }
+  //
+  //   return store
+  // }
+  //
+  // export default configureStore
